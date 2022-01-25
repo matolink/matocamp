c92e3b11d0febcc79308cf5536e168ad9f4c99b1
Matías Gutiérrez <ma.gutierrez.or@gmail.com>
2022-01-25 19:24:33 -0300

    add the numher designation for the pokemons

---
+++ src/pages/pokemones.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react"
+
+const pokemones = () => {
+
+    const [inputUser, setInputUser] = useState(1);
+    const [pokemon, setPokemon] = useState({ nombre: "", tipo1: "", imagen: "" });
+    useEffect(() => {
+        const PKMNURL = "https://pokeapi.co/api/v2/pokemon/" + inputUser
+        fetch(PKMNURL).
+            then(res => res.json()).
+            then(data => setPokemon(
+                {
+                    nombre: data.name,
+                    tipo1: data.types[0].type.name,
+                    imagen: data.sprites.front_default
+                }));
+    },
+    [inputUser]);
+
+
+return (<div>
+    <h1>{pokemon.nombre}</h1>
+    <h2>{pokemon.tipo1}</h2>
+    <img src={pokemon.imagen} />
+    <input type="number" name="" id="" onChange={(eve) => setInputUser(eve.target.value ? eve.target.value : 1)} />
+    <button>go</button>
+</div>);
+}
+
+export default pokemones;
