0e12f00b8cb2214a9c6182ab83a1b903a350d51c
Matías Gutiérrez <ma.gutierrez.or@gmail.com>
2022-01-25 11:32:36 -0300

    added the +1 and -1 buttons

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,16 @@
 import Contenedor from "../components/Contenedor";
 import example from "../../example.json";
+import {useState} from "react"
 
 function matolink() {
+  const Numerito = useState(0)
+  let NumeritoC = Numerito[0]
+  let setNumerito = Numerito[1]
   return (
     <div>
+      <h1>{NumeritoC}</h1>
+      <button onClick={() => setNumerito(NumeritoC + 1)}>mas uno</button>
+      <button onClick={() => setNumerito(NumeritoC - 1)}>menos uno</button>
       {example.map((e) => (
         <div key={e.id}>
           <Contenedor
